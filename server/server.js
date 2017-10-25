2805cbdd8ca16b6d2e7907cc5d93ea65555e039f
Seth McGuire <sether94@live.com>
2017-10-25 16:14:39 -0500

    app

---
+++ server/server.js
@@ -0,0 +1,72 @@
+var express = require('express');
+var app = express();
+var socketIO = require('socket.io');
+var http = require('http');
+var server = http.createServer(app);
+var io = socketIO(server);
+
+var {generateMessage, generateLocationMessage} = require('./utils/message');
+var {isRealString} = require('./utils/validation');
+var {Users} = require('./utils/users');
+var users = new Users();
+
+var path = require('path');
+var publicPath = path.join(__dirname, '../public');
+var port = process.env.PORT || 3000;
+
+app.use(express.static(publicPath));
+
+io.on('connection', function(socket){
+    console.log('New user connected');
+
+    socket.on('join', function(params, callback){
+        if(!isRealString(params.name) || !isRealString(params.room)) {
+           return callback('Name and room name are required');
+        }
+        socket.join(params.room);
+        users.removeUser(socket.id);
+        users.addUser(socket.id, params.name, params.room);
+
+        io.to(params.room).emit('updateUserList', users.getUserList(params.room));
+        socket.emit('newMessage', generateMessage('Admin', 'Welcome to the chat app'));
+        socket.broadcast.to(params.room).emit('newMessage', generateMessage('Admin',  `${params.name} joined`));    
+
+        callback();
+    });
+
+    socket.on('createMessage', function(message, callback){
+        var user = users.getUser(socket.id);
+
+        if(user && isRealString(message.text)){
+            io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));            
+        }
+
+        callback();
+    });
+
+    socket.on('createLocationMessage', function(coords){
+        var user = users.getUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));            
+        }
+    });
+
+    socket.on('disconnect', function(){
+        var user = users.removeUser(socket.id);
+
+        if(user){
+            io.to(user.room).emit('updateUserList', users.getUserList(user.room));
+            io.to(user.room).emit('newMessage', generateMessage('Admin', `${user.name} has left`));
+        }
+    });
+});
+
+
+server.listen(port, function(){
+    console.log(`Server running on ${port}`);
+});
+
+
+//make chatrooms case insensitive
+//make usernames unique
